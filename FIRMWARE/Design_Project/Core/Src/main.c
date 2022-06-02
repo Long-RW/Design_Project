f2f46e17b00abf8bb9a4053bb7bbf4ca80ede260
LongDH20181591 <longx8qd@gmail.com>
2022-06-02 15:15:26 +0700

    Modified FIRMWARE

--- FIRMWARE/Design_Project/Core/Src/main.c
+++ FIRMWARE/Design_Project/Core/Src/main.c
@@ -20,6 +20,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "cmsis_os2.h"
 #include <stdio.h>
 
 /* Private includes ----------------------------------------------------------*/
