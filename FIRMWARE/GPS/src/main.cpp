f3101bcf97a8a5ee511a4fdf2bee18a29677257c
LongDH20181591 <longx8qd@gmail.com>
2022-06-07 15:13:56 +0700

    Add GPS

--- FIRMWARE/GPS/src/main.cpp
+++ FIRMWARE/GPS/src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <ArduinoJson.h>
+
+/******************************* Wifi Setting ***********************************/
+char ssid[] = "";
+char pass[] = "";
+
+/******************************* API *******************************************/
+String key = "My API Key";
+
+/******************** GPS Location Stored Variables ****************************/
+double latitude  = 0.0;
+double longitude = 0.0;
+double accuracy  = 0.0;
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+  Serial.print("Start");
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  Serial.println("Setup done");
+
+  //We start by connecting to a wifi network
+  Serial.print("Connecting to");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(".");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  char bssid
 }
